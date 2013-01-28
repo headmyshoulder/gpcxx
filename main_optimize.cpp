4113dc7bdf957751523fab0c6ac1d12f65e085dd
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-01-28 22:37:32 +0100

    added optimization

---
+++ main_optimize.cpp
@@ -0,0 +1,18 @@
+/*
+ * main_optimize.cpp
+ * Date: 2013-01-28
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ */
+
+#include <iostream>
+
+#define tab "\t"
+
+using namespace std;
+
+
+int main( int argc , char *argv[] )
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
