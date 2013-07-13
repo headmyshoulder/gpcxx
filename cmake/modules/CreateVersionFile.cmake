887ef0b6d56f682775d41cc5fe9752e0e550cc55
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-07-13 21:11:46 +0200

    adding version

--- cmake/modules/CreateVersionFile.cmake
+++ cmake/modules/CreateVersionFile.cmake
@@ -1,15 +1,24 @@
 find_package ( Git )
 
+execute_process ( COMMAND git status -uno
+                  OUTPUT_VARIABLE GP_GIT_CURRENT_STATUS 
+                  OUTPUT_STRIP_TRAILING_WHITESPACE )
+                  
 execute_process ( COMMAND git describe --abbrev=4 HEAD
                   COMMAND sed -e "s/-/./g"
                   OUTPUT_VARIABLE GP_GIT_VERSION
                   OUTPUT_STRIP_TRAILING_WHITESPACE )
 
 
-string ( REGEX REPLACE "^([0-9]+)\\..*" "\\1" GP_VERSION_MAJOR "${GP_GIT_VERSION}" )
-string ( REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" GP_VERSION_MINOR "${GP_GIT_VERSION}" )
-string ( REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" GP_VERSION_PATCH "${GP_GIT_VERSION}" )
-string ( REGEX REPLACE "^[0-9]+\\.[0-9]+\\.[0-9]+.(.*)" "\\1" GP_VERSION_SHA1 "${GP_GIT_VERSION}" )
+string ( REGEX REPLACE "^v([0-9]+)\\..*" "\\1" GP_VERSION_MAJOR "${GP_GIT_VERSION}" )
+string ( REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" GP_VERSION_MINOR "${GP_GIT_VERSION}" )
+string ( REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" GP_VERSION_PATCH "${GP_GIT_VERSION}" )
+string ( REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.[0-9]+.(.*)" "\\1" GP_VERSION_SHA1 "${GP_GIT_VERSION}" )
+
+if ( ${GP_VERSION_PATCH} STREQUAL ${GP_GIT_VERSION} )
+  set ( GP_VERSION_PATCH "0" )
+  set ( GP_VERSION_SHA1 "0" )
+endif ()
 
 set ( GP_VERSION_SHORT "${GP_VERSION_MAJOR}.${GP_VERSION_MINOR}.${GP_VERSION_PATCH}" )
 
@@ -19,5 +28,10 @@ set ( GP_VERSION_SHORT "${GP_VERSION_MAJOR}.${GP_VERSION_MINOR}.${GP_VERSION_PAT
 # message ( STATUS "${GP_VERSION_SHA1}" )
 # message ( STATUS "${GP_VERSION_SHORT}" )
 
-configure_file ( ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp.cmake ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp )
 
+if ( NOT GP_GIT_CURRENT_STATUS STREQUAL "" )
+
+  message ( STATUS "create new config version file for version ${GP_GIT_VERSION}"  )
+  configure_file ( ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp.cmake ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp )
+
+endif ()
