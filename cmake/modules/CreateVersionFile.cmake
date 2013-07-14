8dcdbeffd8d5005544074c0004e4707270357c89
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-07-14 21:15:14 +0200

    fixing version, increment patch number now and adding config_version

--- cmake/modules/CreateVersionFile.cmake
+++ cmake/modules/CreateVersionFile.cmake
@@ -1,8 +1,16 @@
 find_package ( Git )
 
+if ( NOT EXISTS "${CMAKE_SOURCE_DIR}/.git/" )
+  return ()
+endif ()
+
 execute_process ( COMMAND git describe --abbrev=4 HEAD
                   COMMAND sed -e "s/-/./g"
                   OUTPUT_VARIABLE GP_GIT_VERSION
+                  OUTPUT_STRIP_TRAILING_WHITESPACE )
+                  
+execute_process ( COMMAND git status -uno -s 
+                  OUTPUT_VARIABLE GP_GIT_STATUS 
                   OUTPUT_STRIP_TRAILING_WHITESPACE )
 
 
@@ -16,6 +24,8 @@ if ( ${GP_VERSION_PATCH} STREQUAL ${GP_GIT_VERSION} )
   set ( GP_VERSION_SHA1 "0" )
 endif ()
 
+math ( EXPR GP_VERSION_PATCH "${GP_VERSION_PATCH} + 1" )
+
 set ( GP_VERSION_SHORT "${GP_VERSION_MAJOR}.${GP_VERSION_MINOR}.${GP_VERSION_PATCH}" )
 
 # message ( STATUS "${GP_VERSION_MAJOR}" )
@@ -24,5 +34,9 @@ set ( GP_VERSION_SHORT "${GP_VERSION_MAJOR}.${GP_VERSION_MINOR}.${GP_VERSION_PAT
 # message ( STATUS "${GP_VERSION_SHA1}" )
 # message ( STATUS "${GP_VERSION_SHORT}" )
 
+if ( NOT ( GP_GIT_STATUS STREQUAL "" ) )
 
-configure_file ( ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp.cmake ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp )
+  message ( STATUS "generating new config_version.hpp" )
+  configure_file ( ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp.cmake ${CMAKE_SOURCE_DIR}/include/gp/config_version.hpp )
+
+endif ()
---
+++ include/gp/config_version.hpp
@@ -0,0 +1,20 @@
+/*
+  gp/config_version.hpp
+
+  Copyright 2013 Karsten Ahnert
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or
+  copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+
+#ifndef GP_CONFIG_VERSION_HPP_DEFINED
+#define GP_CONFIG_VERSION_HPP_DEFINED
+
+#define GP_VERSION_MAJOR 0
+#define GP_VERSION_MINOR 0
+#define GP_VERSION_PATCH 4
+#define GP_VERSION_SHA1 g6a2d
+
+#endif // GP_CONFIG_VERSION_HPP_DEFINED
