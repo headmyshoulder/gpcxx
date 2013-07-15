0b5893d06e9ee914782869c42ea4879c7706da10
Karsten Ahnert <karsten.ahnert@gmx.de>
2013-07-15 22:00:29 +0200

    adding empty iterate_until

---
+++ include/gp/util/iterate_until.hpp
@@ -0,0 +1,27 @@
+/*
+  gp/util/iterate_until.hpp
+
+  Copyright 2013 Karsten Ahnert
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or
+  copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+
+#ifndef GP_UTIL_ITERATE_UNTIL_HPP_DEFINED
+#define GP_UTIL_ITERATE_UNTIL_HPP_DEFINED
+
+
+namespace gp {
+
+template< typename Seq , typename F >
+bool iterate_until( Seq const& s , F f )
+{
+}
+
+
+} // namespace gp
+
+
+#endif // GP_UTIL_ITERATE_UNTIL_HPP_DEFINED
