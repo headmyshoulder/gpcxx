143fb48b88453cacda23ede8d488c01f6d78d41c
Gerard Choinka <gerard.choinka@ambrosys.de>
2013-12-11 03:23:02 +0100

    Refactoring on the muti thread algorithms and new untitests for it

--- include/gpcxx/util/parallel_algorithm.hpp
+++ include/gpcxx/util/parallel_algorithm.hpp
@@ -22,18 +22,17 @@
 #include <boost/range/adaptor/strided.hpp>
 
 
-#ifndef GPCXX_USE_STD_THREAD_IMPLEMENTATION
-#   define GPCXX_USE_STD_THREAD_IMPLEMENTATION 1
-#endif
 
-#if GPCXX_USE_STD_THREAD_IMPLEMENTATION == 1
+#if GPCXX_USE_THREAD_IMPLEMENTATION == GPCXX_STD_THREAD
     #include <thread>
     typedef std::thread gpcxx_thread_default_impl; 
-    namespace gpcxx { namespace par { auto hardware_concurrency = std::thread::hardware_concurrency; } }
-#else
+    namespace gpcxx { namespace par { auto const hardware_concurrency = std::thread::hardware_concurrency; } }
+#elif GPCXX_USE_THREAD_IMPLEMENTATION == GPCXX_BOOST_THREAD
     #include <boost/thread.hpp>
     typedef boost::thread gpcxx_thread_default_impl;
-    namespace gpcxx { namespace par { auto hardware_concurrency = boost::thread::hardware_concurrency; } }
+    namespace gpcxx { namespace par { auto const hardware_concurrency = boost::thread::hardware_concurrency; } }
+#elif GPCXX_USE_THREAD_IMPLEMENTATION == GPCXX_DUMMY_THREAD
+#   error "GPCXX_DUMMY_THREAD is not implemented"
 #endif
 
     
@@ -98,8 +97,8 @@ std::tuple< size_t, size_t > partition(size_t n_partition,
                                      size_t partition_number,
                                      size_t rng_size )
 {
-//     std::assert( n_partition > 0 );
-//     std::assert( n_partition > partition_number );
+    assert( n_partition > 0 );
+    assert( n_partition > partition_number );
     if( n_partition == 1 )
         return std::tuple<size_t, size_t>{ 0, rng_size };
             
@@ -113,7 +112,7 @@ std::tuple< size_t, size_t > partition(size_t n_partition,
     if ( n_partition  == ( partition_number + 1 ))
         return std::tuple<size_t, size_t>{ partition_size * partition_number, rng_size };
     
-    return std::tuple<size_t, size_t>{ partition_size * partition_number, partition_size * (partition_number + 1) };
+    return std::tuple<size_t, size_t>{ partition_size * partition_number, partition_size * (partition_number + 1)  };
 }
 
 
@@ -131,37 +130,33 @@ output_iterator transform( const single_pass_range1 & rng1,
                            size_t number_of_threads = gpcxx::par::hardware_concurrency()
                         )
 {
-//     std::assert( boost::size(rng1) == boost::size(rng2) );
+    assert( boost::size(rng1) == boost::size(rng2) );
+    
+    if ( boost::empty( rng1 ) )
+        return out;
 
     if ( number_of_threads == 1 )
         return boost::transform( rng1, rng2, out, fun ); 
-    
-    
+   
     size_t rng1_size = boost::size( rng1 );
     
-    if( rng1_size < number_of_threads )
+    if( rng1_size <= number_of_threads )
         number_of_threads = rng1_size;
-    
-    size_t step_size = std::ceil(rng1_size / number_of_threads);
-    
-
-    auto & out_begin_iter = out;
-    auto out_end_iter = advance_with_copy( out,  boost::size( rng1 ) );
-
+        
     std::vector< thread_type >  worker{}; worker.reserve(number_of_threads);
     
     for (size_t i = 0; i < number_of_threads; ++i)
     {
-        size_t rng_begin =     i*step_size;
-        size_t rng_end   = (i+1)*step_size;
-        auto sub_rng1 = rng1 | boost::adaptors::sliced( rng_begin, rng_end );
-        auto sub_rng2 = rng2 | boost::adaptors::sliced( rng_begin, rng_end );
-        auto out_iter = advance_with_copy( out, rng_begin );
+        auto parti =  partition(number_of_threads, i, rng1_size );     
+        auto sub_rng1 = rng1 | boost::adaptors::sliced( std::get<0>(parti), std::get<1>(parti) );
+        auto sub_rng2 = rng2 | boost::adaptors::sliced( std::get<0>(parti), std::get<1>(parti) );
+        auto out_iter = advance_with_copy( out, std::get<0>(parti) );
         
         worker.emplace_back( [=](){ boost::transform( sub_rng1, sub_rng2, out_iter, fun ); } );
     }
     for ( auto& w: worker ) w.join();
-    return out_begin_iter;
+    
+    return out;
 }
 
 
@@ -194,3 +189,4 @@ UnaryFunction for_each(SinglePassRange& rng, UnaryFunction fun, const size_t num
 
 
 #endif // GPCXX_PARALLEL_ALGORITHM_HPP_DEFINED
+class name;
---
+++ test/util/parallel_algorithm.cpp
@@ -0,0 +1,44 @@
+/*
+ * parallel_algorithm.cpp
+ * Date: 2013-12-10
+ * Author: Gerard Choinka (gerard.choinka@ambrosys.de)
+ */
+
+#include <gpcxx/util/parallel_algorithm.hpp>
+#include <boost/range.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
+#include <cstdint>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+#define TESTNAME parallel_algorithm
+
+TEST( TESTNAME , transform_unary )
+{
+    size_t thread_count[] = { 2, 3, 4, 5, 7, 8, 9, 10 };
+    auto rng_size_min = 1, rng_size_max = 21, rng_size_step=1;
+    
+    for( auto const nthreads : thread_count )
+    {
+        for( auto i = rng_size_min; i < rng_size_max; i+= rng_size_step)
+        {
+            int k = 0;
+            vector<size_t> rng1; boost::generate(rng1, [&](){ return k++;});
+            k=0;
+            vector<size_t> rng2; boost::generate(rng2, [&](){ return k++;});
+            
+           
+            auto const fun = [](size_t a, size_t b) { return a * b; };
+            std::vector<int> out_singel_thread( boost::size( rng1 ), 0 );
+            std::vector<int> out_n_thread( boost::size( rng1 ), 1 );
+            
+            boost::transform( rng1, rng2, boost::begin( out_singel_thread ), fun );
+            gpcxx::par::transform( rng1, rng2, boost::begin( out_n_thread ), fun, 2 );
+            EXPECT_EQ( out_singel_thread , out_n_thread );
+        }
+    }
+}
+
