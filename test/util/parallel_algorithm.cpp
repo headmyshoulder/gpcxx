e12b7afd2ba53a667fde7c1846592f5f60c2265e
Gerard Choinka <gerard.choinka@ambrosys.de>
2013-12-11 03:34:53 +0100

    Improve unittest transform_unary

--- test/util/parallel_algorithm.cpp
+++ test/util/parallel_algorithm.cpp
@@ -8,6 +8,7 @@
 #include <boost/range.hpp>
 #include <boost/range/irange.hpp>
 #include <boost/range/algorithm_ext/push_back.hpp>
+#include <algorithm>
 #include <cstdint>
 
 #include <gtest/gtest.h>
@@ -23,21 +24,24 @@ TEST( TESTNAME , transform_unary )
     
     for( auto const nthreads : thread_count )
     {
-        for( auto i = rng_size_min; i < rng_size_max; i+= rng_size_step)
+        for( auto i = rng_size_min; i < rng_size_max; i += rng_size_step)
         {
             int k = 0;
-            vector<size_t> rng1; boost::generate(rng1, [&](){ return k++;});
+            vector<size_t> rng1; boost::generate( rng1, [&](){ return k++; } );
             k=0;
-            vector<size_t> rng2; boost::generate(rng2, [&](){ return k++;});
+            vector<size_t> rng2; boost::generate( rng2, [&](){ return k++; } );
             
            
             auto const fun = [](size_t a, size_t b) { return a * b; };
-            std::vector<int> out_singel_thread( boost::size( rng1 ), 0 );
-            std::vector<int> out_n_thread( boost::size( rng1 ), 1 );
+            std::vector<int> out_singel_thread_boost ( boost::size( rng1 ), 0 );
+            std::vector<int> out_n_thread_gpcxx_par ( boost::size( rng1 ), 1 );
+            std::vector<int> out_singel_thread_std ( boost::size( rng1 ), 3 );
             
-            boost::transform( rng1, rng2, boost::begin( out_singel_thread ), fun );
-            gpcxx::par::transform( rng1, rng2, boost::begin( out_n_thread ), fun, 2 );
-            EXPECT_EQ( out_singel_thread , out_n_thread );
+            boost::transform( rng1, rng2, boost::begin( out_singel_thread_boost ), fun );
+            gpcxx::par::transform( rng1, rng2, boost::begin( out_n_thread_gpcxx_par ), fun, nthreads );
+            std::transform( boost::begin( rng1 ), boost::end( rng1 ), boost::begin( rng2 ), boost::begin( out_singel_thread_std ), fun );
+            EXPECT_EQ( out_singel_thread_boost , out_n_thread_gpcxx_par );
+            EXPECT_EQ( out_singel_thread_std   , out_n_thread_gpcxx_par );            
         }
     }
 }
