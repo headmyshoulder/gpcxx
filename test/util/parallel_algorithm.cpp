bb40747bb7008b32c5ca81b25175825ff5e5b892
Gerard Choinka <gerard.choinka@ambrosys.de>
2013-12-12 14:29:42 +0100

    Add a test for muti thread for_each

--- test/util/parallel_algorithm.cpp
+++ test/util/parallel_algorithm.cpp
@@ -8,8 +8,10 @@
 #include <boost/range.hpp>
 #include <boost/range/irange.hpp>
 #include <boost/range/algorithm_ext/push_back.hpp>
+#include <boost/range/algorithm.hpp>
 #include <algorithm>
 #include <cstdint>
+#include <mutex>
 
 #include <gtest/gtest.h>
 
@@ -56,19 +58,54 @@ TEST( TESTNAME , transform_unary )
         for( auto i = rng_size_min; i < rng_size_max; i += rng_size_step)
         {
             int k = 0;
-            vector<size_t> rng1; boost::generate( rng1, [&](){ return k++; } );
+            vector<size_t> rng; boost::generate( rng, [&](){ return k++; } );
             
            
             auto const fun = [](size_t a) { return a * a; };
-            std::vector<int> out_singel_thread_boost ( boost::size( rng1 ), 0 );
-            std::vector<int> out_n_thread_gpcxx_par ( boost::size( rng1 ), 1 );
-            std::vector<int> out_singel_thread_std ( boost::size( rng1 ), 3 );
+            std::vector<int> out_singel_thread_boost ( boost::size( rng ), 0 );
+            std::vector<int> out_n_thread_gpcxx_par ( boost::size( rng ), 1 );
+            std::vector<int> out_singel_thread_std ( boost::size( rng ), 3 );
             
-            boost::transform( rng1, boost::begin( out_singel_thread_boost ), fun );
-            gpcxx::par::transform( rng1, boost::begin( out_n_thread_gpcxx_par ), fun, nthreads );
-            std::transform( boost::begin( rng1 ), boost::end( rng1 ), boost::begin( out_singel_thread_std ), fun );
+            boost::transform( rng, boost::begin( out_singel_thread_boost ), fun );
+            gpcxx::par::transform( rng, boost::begin( out_n_thread_gpcxx_par ), fun, nthreads );
+            std::transform( boost::begin( rng ), boost::end( rng ), boost::begin( out_singel_thread_std ), fun );
             EXPECT_EQ( out_singel_thread_boost , out_n_thread_gpcxx_par );
             EXPECT_EQ( out_singel_thread_std   , out_n_thread_gpcxx_par );            
+        }
+    }
+}
+
+TEST( TESTNAME , for_each )
+{
+    size_t thread_count[] = { 2, 3, 4, 5, 7, 8, 9, 10 };
+    auto rng_size_min = 1, rng_size_max = 21, rng_size_step=1;
+    
+    for( auto const nthreads : thread_count )
+    {
+        for( auto i = rng_size_min; i < rng_size_max; i += rng_size_step)
+        {
+            int k = 0;
+            vector<size_t> rng; 
+            boost::generate( rng, [&](){ return k++; } );
+                    
+            size_t sum_singel_thread_boost = 0;
+            size_t sum_n_thread_gpcxx_par = 0;
+            size_t sum_singel_thread_std = 0;
+            std::mutex mu;
+            
+            auto const fun = make_tuple(
+                [&](size_t a) { sum_singel_thread_boost += a; },
+                [&](size_t a) { std::lock_guard<std::mutex> lg(mu); sum_n_thread_gpcxx_par += a; },
+                [&](size_t a) { sum_singel_thread_std += a; }
+                             );
+            
+            
+            
+            boost::for_each( rng, std::get<0>(fun) );
+            gpcxx::par::for_each( rng, std::get<1>(fun), nthreads );
+            std::for_each( boost::begin( rng ), boost::end( rng ), std::get<2>(fun) );
+            EXPECT_EQ( sum_singel_thread_boost , sum_n_thread_gpcxx_par );
+            EXPECT_EQ( sum_singel_thread_std   , sum_n_thread_gpcxx_par );  
         }
     }
 }
