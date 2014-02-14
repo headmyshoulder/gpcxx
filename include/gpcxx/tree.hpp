5599e8375c93b1747e8dba5713425e70484cc906
Karsten Ahnert <karsten.ahnert@gmx.de>
2014-02-14 10:36:46 +0100

    adding files to tree.hpp

--- include/gpcxx/tree.hpp
+++ include/gpcxx/tree.hpp
@@ -15,6 +15,11 @@
 
 #include <gpcxx/tree/cursor_traits.hpp>
 #include <gpcxx/tree/basic_tree.hpp>
+#include <gpcxx/tree/intrusive_tree.hpp>
+#include <gpcxx/tree/intrusive_node.hpp>
+#include <gpcxx/tree/intrusive_func.hpp>
+#include <gpcxx/tree/basic_intrusive_node.hpp>
+#include <gpcxx/tree/basic_named_intrusive_node.hpp>
 
 
 #endif // GPCXX_TREE_HPP_DEFINED
