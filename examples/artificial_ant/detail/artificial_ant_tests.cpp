eab49cf84204f33baca9c7494b8a5efcb9197015
Gerard Choinka <gerard.choinka@ambrosys.de>
2014-10-13 14:46:35 +0200

    Renamed filenames

--- examples/artificial_ant/detail/artificial_ant_tests.cpp
+++ examples/artificial_ant/detail/artificial_ant_tests.cpp
@@ -14,6 +14,7 @@
 #include "santa_fe_trail.hpp"
 #include <gpcxx/io.hpp>
 #include <iostream>
+#include <utility>
 
 char const newl = '\n';
 
@@ -35,6 +36,52 @@ bool ant_move_test()
     return position_is_valid && step_count_is_valid ;
 } 
 //]
+
+void  paper_tree()
+{
+    using namespace ant_example;
+    typedef typename tree_type::node_type intrusive_node_type;
+    typedef std::pair< size_t , std::function< intrusive_node_type() > >
+            intrusive_mapped_func;
+    typedef std::unordered_map< std::string , intrusive_mapped_func >
+            intrusive_mapper_map_type;
+    typedef std::function< intrusive_mapped_func( std::string const& ) >
+            intrusive_mapper_type;
+
+    intrusive_mapper_map_type mm = intrusive_mapper_map_type {
+            std::make_pair( "if" , std::make_pair( 2 , []() { return intrusive_node_type(
+                    if_food_ahead {} , "if" ); } ) ) ,
+            std::make_pair( "p2" , std::make_pair( 2 , []() { return intrusive_node_type(
+                    prog2 {} , "p2" ); } ) ) ,
+            std::make_pair( "p3" , std::make_pair( 3 , []() { return intrusive_node_type(
+                    prog3 {} , "p3" ); } ) ) ,
+            std::make_pair( "m" , std::make_pair( 0 , []() { return intrusive_node_type(
+                    ant_move_task_terminal {} , "m" ); } ) ) ,
+            std::make_pair( "l" , std::make_pair( 0 , []() { return intrusive_node_type(
+                    ant_turn_left_task_terminal {} , "l" ); } ) ) ,
+            std::make_pair( "r" , std::make_pair( 0 , []() { return intrusive_node_type(
+                    ant_turn_right_task_terminal {} , "r" ); } ) )
+    };
+    intrusive_mapper_type mapper = [mm]( std::string const& str ) {
+        return mm.find( str )->second; };
+
+    std::string koza2 = "if|m|p3|r|if|m|l|p3|l|if|m|r|m";
+    tree_type tree;
+    gpcxx::read_polish( koza2 , tree , mapper );
+
+    board const b{ santa_fe::x_size, santa_fe::y_size };
+    int const max_steps { 400 };
+
+    {
+        ant_simulation ant_sim{santa_fe::make_santa_fe_trail(b), santa_fe::x_size, santa_fe::y_size, {0, 0}, east, max_steps};
+        while (!ant_sim.is_finsh())
+        {
+            tree.root()->eval(ant_sim);
+        }
+        std::cout << "Koza2:" <<  ant_sim.score() << "\n";
+    }
+
+}
 
 
 int main( int argc , char *argv[] )
@@ -97,57 +144,40 @@ int main( int argc , char *argv[] )
 
         while (!ant_sim.is_finsh())
         {
-            std::cout << "NEW Run, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
             if(ant_sim.food_in_front())
             {
-                std::cout << "Food in front, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                 ant_sim.move();
-                std::cout << "Moved, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
             }
             else
             {//PROG3
-                std::cout << "NO Food in front, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                 ant_sim.turn_left();
-                std::cout << "Left, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                 {//PROG2
                     if(ant_sim.food_in_front())
                     {
-                        std::cout << "Food in front, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                         ant_sim.move();
-                        std::cout << "Moved, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                     }
                     else
                     {
-                        std::cout << "NO Food in front, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                         ant_sim.turn_right();
-                        std::cout << "Right, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                     }
                     {//PROG2
                         ant_sim.turn_right();
-                        std::cout << "Right, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                         {//PROG2
                             ant_sim.turn_left();
-                            std::cout << "Left, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                             ant_sim.turn_right();
-                            std::cout << "Right, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                         }
                     }
                 }
                 {//PROG2
                     if(ant_sim.food_in_front())
                     {
-                        std::cout << "Food in front, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                         ant_sim.move();
-                        std::cout << "Moved, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                     }
                     else
                     {
-                        std::cout << "NO Food in front, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                         ant_sim.turn_left();
-                        std::cout << "Left, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                     }
                     ant_sim.move();
-                    std::cout << "Moved, i have done " << ant_sim.steps_done() << "steps, score " << ant_sim.score() << ", my pos " << ant_sim.ant_position() << ":" << ant_sim.ant_direction() << newl;
                 }
             }
         }
@@ -155,6 +185,8 @@ int main( int argc , char *argv[] )
         std::cout << "Manual:" << ant_sim.score() << "\n";
     }
 
+
+    paper_tree();
 
 
     int count = 0;
