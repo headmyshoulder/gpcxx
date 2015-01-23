6f69478c812770e1f56ff8ae33ececc13f2c7021
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-01-23 19:48:56 +0100

    adding (empty) files for iterators and according test files

---
+++ include/gpcxx/tree/iterator/iterator_base.hpp
@@ -0,0 +1,24 @@
+/*
+ * gpcxx/tree/iterator/iterator_base.hpp
+ * Date: 2015-01-22
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ * Copyright: Karsten Ahnert
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or
+ * copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef GPCXX_TREE_ITERATOR_ITERATOR_BASE_HPP_INCLUDED
+#define GPCXX_TREE_ITERATOR_ITERATOR_BASE_HPP_INCLUDED
+
+
+namespace gpcxx {
+
+
+
+
+} // namespace gpcxx
+
+
+#endif // GPCXX_TREE_ITERATOR_ITERATOR_BASE_HPP_INCLUDED
---
+++ include/gpcxx/tree/iterator/postorder_iterator.hpp
@@ -0,0 +1,24 @@
+/*
+ * gpcxx/tree/iterator/postorder_iterator.hpp
+ * Date: 2015-01-22
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ * Copyright: Karsten Ahnert
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or
+ * copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef GPCXX_TREE_ITERATOR_POSTORDER_ITERATOR_HPP_INCLUDED
+#define GPCXX_TREE_ITERATOR_POSTORDER_ITERATOR_HPP_INCLUDED
+
+
+namespace gpcxx {
+
+
+
+
+} // namespace gpcxx
+
+
+#endif // GPCXX_TREE_ITERATOR_POSTORDER_ITERATOR_HPP_INCLUDED
