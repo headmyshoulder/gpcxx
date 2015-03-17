a6e7dad79126079d6badfb108250c4881b5ba8ba
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-03-17 12:38:03 +0100

    ...

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -2,8 +2,8 @@ set -x
 set -e
 
 cd build
-lcov --zerocounters --directory test
-lcov --initial --directory test --base-directory ../include/gpcxx --capture --output-file coverage.info
+rm `find ./test -name "*.gcda"`
+rm `find ./test -name "*.gcno"`
 ctest 
 lcov --directory test --base-directory ../include/gpcxx --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '*/third_party/*' -o coverage.info
