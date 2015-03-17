9c814511c2d8c24e027c93b679099bb78a21a7cd
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-03-17 07:12:25 +0100

    enabling coverity

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -1,4 +1,6 @@
 set -x
 set -e
 
-echo "Hello world."
+lcov --directory build/tests --base-directory include --capture --output-file coverage.info
+lcov --remove coverage.info '/usr*' '*/cl.hpp' -o coverage.info
+coveralls-lcov build/coverage.info
--- .ci/coveralls.sh
+++
@@ -1,6 +0,0 @@
-set -x
-set -e
-
-lcov --directory build/tests --base-directory include --capture --output-file coverage.info
-lcov --remove coverage.info '/usr*' '*/cl.hpp' -o coverage.info
-coveralls-lcov build/coverage.info
