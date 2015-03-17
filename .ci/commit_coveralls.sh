3108fd955b02e580d792415f32b1f93fe3fee7ad
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-03-17 10:01:13 +0100

    fixign coverall

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -1,6 +1,9 @@
 set -x
 set -e
 
-lcov --directory build/test --base-directory include/gpcxx --capture --output-file coverage.info
+cd build
+make coverage_reset
+ctest 
+lcov --directory test --base-directory ../include/gpcxx --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '*/third_party/*' -o coverage.info
 coveralls-lcov coverage.info
