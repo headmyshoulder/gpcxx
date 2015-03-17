e287bf7a909522ba0adf822555c7a5f7791f7b9c
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-03-17 17:07:00 +0100

    fixing coveralls

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -2,10 +2,7 @@ set -x
 set -e
 
 cd build
-# rm `find ./test -name "*.gcda"`
-# rm `find ./test -name "*.gcno"`
-# ctest 
-lcov --zerocounters --directory test
+# lcov --zerocounters --directory test
 lcov --directory test --base-directory ../include/gpcxx --capture --output-file coverage.info
-lcov --remove coverage.info '/usr*' '*/third_party/*' -o coverage.info
+lcov --remove coverage.info '/usr*' '*/third_party*' -o coverage.info
 coveralls-lcov coverage.info
