e3d092aeb64d33f7c01a4b296af59d059c15b27b
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-03-17 10:21:58 +0100

    ...

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 cd build
-make coverage_reset
+lcov --zerocounters --directory test
 ctest 
 lcov --directory test --base-directory ../include/gpcxx --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '*/third_party/*' -o coverage.info
