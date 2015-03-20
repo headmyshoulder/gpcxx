c880172c74f0da2757e30642617adb90afd50ae2
Coverity Scan Builder <travis@travis-ci.org>
2015-03-20 12:53:02 +0100

    enabling automatic coverity scan report

--- .ci/commit_coverity_scan.sh
+++ .ci/commit_coverity_scan.sh
@@ -1,4 +1,20 @@
 set -x
 set -e
 
-echo "Hello world."
+REPO_PATH="https://headmyshoulder:${GIT_TOKEN}@github.com/Ambrosys/gpcxx.git"
+COVERITY_PATH=coverity
+COMMIT_USER="Coverity Scan Builder"
+COMMIT_EMAIL="travis@travis-ci.org"
+
+git config user.name "${COMMIT_USER}"
+git config user.email "${COMMIT_EMAIL}"
+
+# Get a clean version of the repo.
+rm -rf ${COVERITY_PATH}
+mkdir -p ${COVERITY_PATH}
+git clone -b coverity_scan "${REPO_PATH}" --single-branch ${COVERITY_PATH}
+
+
+cd ${COVERITY_PATH}
+git pull --no-edit origin master
+git push origin coverity_scan
