5ddb40578dc87a1d9e5808a88d70080bd78fcbbc
Coverity Scan Builder <travis@travis-ci.org>
2015-03-20 13:29:52 +0100

    starting with automatically building the documentation

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -1,4 +1,29 @@
 set -x
 set -e
 
-echo "Hello world."
+cd $BOOST_ROOT/tools/quickbook
+../../b2 dist-bin
+export $QUICKBOOK_ROOT=$BOOST_ROOT/dist/bin
+export $BOOSTBOOK_ROOT=$BOOST_ROOT/tools/boostbook
+export $DOCBOOK_ROOT=$GPCXX_ROOT/third_party/doc_book
+
+mkdir $DOCBOOK_ROOT
+cd $DOCBOOK_ROOT
+
+mkdir docbook-xml
+cd docbook-xml
+wget http://www.oasis-open.org/docbook/xml/4.2/docbook-xml-4.2.zip
+unzip docbook-xml-4.2.zip
+rm docbook-xml-4.2.zip
+cd ..
+
+wget http://sourceforge.net/projects/docbook/files/docbook-xsl/1.78.1/docbook-xsl-1.78.1.tar.bz2/download -O docbook-xsl-1.78.1.tar.bz2
+tar xvfj docbook-xsl-1.78.1.tar.bz2
+ln -s docbook-xsl-1.78.1 docbook-xsl
+rm docbook-xsl-1.78.1.tar.bz2
+
+cd $GPCXX_ROOT
+cd build
+cmake .. -DGPCXX_BUILD_DOCS
+make documentation
+
