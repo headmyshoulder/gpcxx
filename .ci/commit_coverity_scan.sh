e975592054e28bd468a9b33125a9b557e4450539
Documentation Builder <travis@travis-ci.org>
2015-03-25 20:46:30 +0100

    fixing coverity build

--- .ci/commit_coverity_scan.sh
+++ .ci/commit_coverity_scan.sh
@@ -1,19 +1,26 @@
 set -x
 set -e
 
-COVERITY_PATH=coverity
-COMMIT_USER="Coverity Scan Builder"
-COMMIT_EMAIL="travis@travis-ci.org"
+# COVERITY_PATH=coverity
+# COMMIT_USER="Coverity Scan Builder"
+# COMMIT_EMAIL="travis@travis-ci.org"
 
 # Get a clean version of the repo.
-rm -rf ${COVERITY_PATH}
-mkdir -p ${COVERITY_PATH}
-git clone -b coverity_scan "${REPO_PATH}" --single-branch ${COVERITY_PATH}
+# rm -rf ${COVERITY_PATH}
+# mkdir -p ${COVERITY_PATH}
+# git clone -b coverity_scan "${REPO_PATH}" --single-branch ${COVERITY_PATH}
 
 
-cd ${COVERITY_PATH}
-git config user.name "${COMMIT_USER}"
-git config user.email "${COMMIT_EMAIL}"
+# cd ${COVERITY_PATH}
+# git config user.name "${COMMIT_USER}"
+# git config user.email "${COMMIT_EMAIL}"
 
-git pull --no-edit origin master
-git push origin coverity_scan
+# git pull --no-edit origin master
+# git push origin coverity_scan
+
+COVERITY_SCAN_PROJECT_NAME="Ambosys/gpcxx"
+COVERITY_SCAN_NOTIFICATION_EMAIL="karsten.ahnert@gmx.de"
+COVERITY_SCAN_BUILD_COMMAND="make"
+COVERITY_SCAN_BUILD_COMMAND_PREPEND="mkdir coverity_build && cd coverity_build && cmake .. -DGPCXX_BUILD_DOCS=OFF"
+COVERITY_SCAN_BRANCH_PATTERN="master"
+curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh |bash
