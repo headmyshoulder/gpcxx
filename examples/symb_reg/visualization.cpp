0c98043453e4469c2e4bafc33f05cd76fefdadcc
Documentation Builder <travis@travis-ci.org>
2015-05-03 07:43:26 +0200

    writing height in visualization and writing fitness to key fitness instead of key value

--- examples/symb_reg/visualization.cpp
+++ examples/symb_reg/visualization.cpp
@@ -75,7 +75,7 @@ struct evolution_part
 
 struct operator_observer
 {
-    using population_type = std::vector< std::tuple< double , std::string , size_t > >;
+    using population_type = std::vector< std::tuple< double , std::string , size_t , size_t > >;
     using population_vector = std::vector< population_type >;
     
     using evolution_type = std::vector< evolution_part >;
@@ -105,7 +105,7 @@ struct operator_observer
         m_populations.push_back( population_type {} );
         for( size_t i=0 ; i<population.size() ; ++i )
         {
-            m_populations.back().push_back( std::make_tuple( fitness[i] , gpcxx::simple_string( population[i] ) , population[i].size() ) );
+            m_populations.back().push_back( std::make_tuple( fitness[i] , gpcxx::simple_string( population[i] ) , population[i].size() , population[i].height() ) );
         }
         
         m_evolutions.push_back( evolution_type {} );
@@ -146,8 +146,9 @@ struct operator_observer
                 out << "\"generation\" : " << i << " , ";
                 out << "\"name\" : \"" << std::to_string( j + 1 )<< "\" , ";
                 out << "\"formula\" : \"" << std::get< 1 >( p ) << "\" , ";
-                out << "\"value\" : "; write_double( out , std::get< 0 >( p ) ); out << " , ";
-                out << "\"size\" : "; write_int( out , std::get< 2 >( p ) ); out << " ";
+                out << "\"fitness\" : "; write_double( out , std::get< 0 >( p ) ); out << " , ";
+                out << "\"size\" : "; write_int( out , std::get< 2 >( p ) ); out << " , ";
+                out << "\"height\" : "; write_int( out , std::get< 3 >( p ) ); out << " ";
                 out << "}";
             }
         }
@@ -271,8 +272,8 @@ int main( int argc , char *argv[] )
     //]
 
     //[ define_gp_parameters
-    size_t population_size = 256;
-    size_t generation_size = 12;
+    size_t population_size = 64;  // 256
+    size_t generation_size = 6;    // 12
     size_t number_elite = 2;
     double mutation_rate = 0.3;
     double crossover_rate = 0.6;
