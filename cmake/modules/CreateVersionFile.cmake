5b2930a15b07031fd30b6a6aecf449f7ca43c939
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-05-21 16:09:28 +0200

    adding cmake debug information about git describe

--- cmake/modules/CreateVersionFile.cmake
+++ cmake/modules/CreateVersionFile.cmake
@@ -15,6 +15,8 @@ execute_process ( COMMAND git describe --tags --abbrev=4 --match v*.* HEAD
                   OUTPUT_VARIABLE GPCXX_GIT_VERSION
                   OUTPUT_STRIP_TRAILING_WHITESPACE
                   WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/" )
+
+message ( STATUS "${GPCXX_GIT_VERSION}" )
                   
 execute_process ( COMMAND git status -uno -s 
                   OUTPUT_VARIABLE GPCXX_GIT_STATUS 
