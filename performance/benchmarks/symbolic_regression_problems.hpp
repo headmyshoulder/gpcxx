b3d9816326b17d4bf5a60db7ef946e94af436589
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-06-15 06:29:01 +0200

    fixing clang warning in symbolic regression problems

--- performance/benchmarks/symbolic_regression_problems.hpp
+++ performance/benchmarks/symbolic_regression_problems.hpp
@@ -20,69 +20,69 @@
 
 template< typename Rng >
 auto generate_koza1( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return  x * x * x * x + x * x * x + x * x + x; } ); }
 
 template< typename Rng >
 auto generate_koza2( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
             return x * x * x * x * x - 2.0 * x * x * x + x; } ); }
 
 template< typename Rng >
 auto generate_koza3( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return x * x * x * x * x * x - 2.0 * x * x * x * x + x * x;  } ); }
 
 
 
 template< typename Rng >
 auto generate_nguyen1( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return x * x * x + x * x + x; } ); }
 
 template< typename Rng >
 auto generate_nguyen2( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return x * x * x * x + x * x * x + x * x + x; } ); }
 
 template< typename Rng >
 auto generate_nguyen3( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return x * x * x * x * x + x * x * x * x + x * x * x + x * x + x; } ); }
 
 template< typename Rng >
 auto generate_nguyen4( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return x * x * x * x * x *x + x * x * x * x * x + x * x * x * x + x * x * x + x * x + x; } ); }
 
 template< typename Rng >
 auto generate_nguyen5( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return sin( x * x ) * cos( x ) - 1.0; } ); }
 
 template< typename Rng >
 auto generate_nguyen6( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( -1.0 , 1.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( -1.0 , 1.0 ) }} , []( double x ) {
         return sin( x ) + sin( x + x * x ); } ); }
 
 template< typename Rng >
 auto generate_nguyen7( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( 0.0 , 2.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( 0.0 , 2.0 ) }} , []( double x ) {
         return log( x + 1.0 ) + log( x * x + 1.0 ); } ); }
 
 template< typename Rng >
 auto generate_nguyen8( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , { std::make_pair( 0.0 , 4.0 ) } , []( double x ) {
+    return gpcxx::generate_uniform_distributed_test_data< 1 >( rng , 20 , {{ std::make_pair( 0.0 , 4.0 ) }} , []( double x ) {
         return sqrt( x ); } ); }
 
 template< typename Rng >
 auto generate_nguyen9( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 2 >( rng , 100 , { std::make_pair( -1.0 , 1.0 ) , std::make_pair( -1.0 , 1.0 ) } , []( double x , double y ) {
+    return gpcxx::generate_uniform_distributed_test_data< 2 >( rng , 100 , {{ std::make_pair( -1.0 , 1.0 ) , std::make_pair( -1.0 , 1.0 ) }} , []( double x , double y ) {
         return sin( x ) + cos( x * x ); } ); }
 
 template< typename Rng >
 auto generate_nguyen10( Rng& rng ) {
-    return gpcxx::generate_uniform_distributed_test_data< 2 >( rng , 100 , { std::make_pair( -1.0 , 1.0 ) , std::make_pair( -1.0 , 1.0 ) } , []( double x , double y ) {
+    return gpcxx::generate_uniform_distributed_test_data< 2 >( rng , 100 , {{ std::make_pair( -1.0 , 1.0 ) , std::make_pair( -1.0 , 1.0 ) }} , []( double x , double y ) {
         return 2 * sin( x ) * cos( y ); } ); }
 
 
