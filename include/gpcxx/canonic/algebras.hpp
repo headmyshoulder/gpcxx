f25191106272fb09c41a6fa602f5c7ac39d08b68
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-09-02 08:37:52 +0200

    starting with canonicalization, adding empty files

---
+++ include/gpcxx/canonic/algebras.hpp
@@ -0,0 +1,24 @@
+/*
+ * gpcxx/canonic/algebras.hpp
+ * Date: 2015-09-02
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ * Copyright: Karsten Ahnert
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or
+ * copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef GPCXX_CANONIC_ALGEBRAS_HPP_INCLUDED
+#define GPCXX_CANONIC_ALGEBRAS_HPP_INCLUDED
+
+
+namespace gpcxx {
+
+
+
+
+} // namespace gpcxx
+
+
+#endif // GPCXX_CANONIC_ALGEBRAS_HPP_INCLUDED
---
+++ test/canonic/group.cpp
@@ -0,0 +1,22 @@
+/*
+ * test/canonic/group.cpp
+ * Date: 2015-09-02
+ * Author: Karsten Ahnert (karsten.ahnert@gmx.de)
+ * Copyright: Karsten Ahnert
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or
+ * copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <sstream>
+#include <gtest/gtest.h>
+
+#define TESTNAME group_tests
+
+using namespace std;
+
+TEST( TESTNAME , TestCase )
+{
+    EXPECT_EQ( true , true );
+}
